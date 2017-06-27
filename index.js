4d9c0626c0476f1910419f59fb8c944efacbc0b5
Andrew Dover <andy.dover@dna.co.nz>
2017-06-28 09:25:14 +1200

    FIX: Readded CLI back in

--- index.js
+++ index.js
@@ -24,6 +24,7 @@
 
 const chalk = require('chalk');
 const clear = require('clear');
+const CLI = require('clui');
 const figlet = require('figlet');
 const inquirer = require('inquirer');
 const Preferences = require('preferences');
